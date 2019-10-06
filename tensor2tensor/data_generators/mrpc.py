da53c19b77e604486a9c759c51e2e2f6a29ba3a0
John Ed Alvinez <je.alvinez@gmail.com>
2019-10-06 18:14:14 +1100

    [FEATURE] Add MSRParaphraseCorpusGenerateParaphrase problem

--- tensor2tensor/data_generators/mrpc.py
+++ tensor2tensor/data_generators/mrpc.py
@@ -27,23 +27,41 @@ from tensor2tensor.data_generators import text_problems
 from tensor2tensor.utils import registry
 import tensorflow as tf
 
+# Link to data from GLUE: https://gluebenchmark.com/tasks
+_DEV_IDS = ("https://firebasestorage.googleapis.com/v0/b/"
+            "mtl-sentence-representations.appspot.com/o/"
+            "data%2Fmrpc_dev_ids.tsv?alt=media&token=ec5c0836-31d5-"
+            "48f4-b431-7480817f1adc")
+_MRPC_TRAIN = ("https://dl.fbaipublicfiles.com/senteval/senteval_data/"
+              "msr_paraphrase_train.txt")
+_MRPC_TEST = ("https://dl.fbaipublicfiles.com/senteval/senteval_data/"
+              "msr_paraphrase_test.txt")
+_DATA_DIR = "MRPC"
+
 EOS = text_encoder.EOS
+
+
+def _maybe_download_corpora(tmp_dir):
+  mrpc_dir = os.path.join(tmp_dir, _DATA_DIR)
+  tf.gfile.MakeDirs(mrpc_dir)
+  mrpc_train_finalpath = os.path.join(mrpc_dir, "msr_paraphrase_train.txt")
+  mrpc_test_finalpath = os.path.join(mrpc_dir, "msr_paraphrase_test.txt")
+  mrpc_dev_ids_finalpath = os.path.join(mrpc_dir, "dev_ids.tsv")
+
+  def download_file(tdir, filepath, url):
+    if not tf.gfile.Exists(filepath):
+      generator_utils.maybe_download(tdir, filepath, url)
+
+  download_file(mrpc_dir, mrpc_train_finalpath, _MRPC_TRAIN)
+  download_file(mrpc_dir, mrpc_test_finalpath, _MRPC_TEST)
+  download_file(mrpc_dir, mrpc_dev_ids_finalpath, _DEV_IDS)
+
+  return mrpc_dir
 
 
 @registry.register_problem
 class MSRParaphraseCorpus(text_problems.TextConcat2ClassProblem):
   """MSR Paraphrase Identification problems."""
-
-  # Link to data from GLUE: https://gluebenchmark.com/tasks
-  DEV_IDS = ("https://firebasestorage.googleapis.com/v0/b/"
-             "mtl-sentence-representations.appspot.com/o/"
-             "data%2Fmrpc_dev_ids.tsv?alt=media&token=ec5c0836-31d5-"
-             "48f4-b431-7480817f1adc")
-  MRPC_TRAIN = ("https://s3.amazonaws.com/senteval/senteval_data/"
-                "msr_paraphrase_train.txt")
-  MRPC_TEST = ("https://s3.amazonaws.com/senteval/senteval_data/"
-               "msr_paraphrase_test.txt")
-  DATA_DIR = "MRPC"
 
   @property
   def is_generate_per_split(self):
@@ -74,23 +92,6 @@ class MSRParaphraseCorpus(text_problems.TextConcat2ClassProblem):
     del data_dir
     return ["not_paraphrase", "paraphrase"]
 
-  def _maybe_download_corpora(self, tmp_dir):
-    mrpc_dir = os.path.join(tmp_dir, self.DATA_DIR)
-    tf.gfile.MakeDirs(mrpc_dir)
-    mrpc_train_finalpath = os.path.join(mrpc_dir, "msr_paraphrase_train.txt")
-    mrpc_test_finalpath = os.path.join(mrpc_dir, "msr_paraphrase_test.txt")
-    mrpc_dev_ids_finalpath = os.path.join(mrpc_dir, "dev_ids.tsv")
-
-    def download_file(tdir, filepath, url):
-      if not tf.gfile.Exists(filepath):
-        generator_utils.maybe_download(tdir, filepath, url)
-
-    download_file(mrpc_dir, mrpc_train_finalpath, self.MRPC_TRAIN)
-    download_file(mrpc_dir, mrpc_test_finalpath, self.MRPC_TEST)
-    download_file(mrpc_dir, mrpc_dev_ids_finalpath, self.DEV_IDS)
-
-    return mrpc_dir
-
   def example_generator(self, filename, dev_ids, dataset_split):
     for idx, line in enumerate(tf.gfile.Open(filename, "rb")):
       if idx == 0: continue  # skip header
@@ -109,7 +110,7 @@ class MSRParaphraseCorpus(text_problems.TextConcat2ClassProblem):
         }
 
   def generate_samples(self, data_dir, tmp_dir, dataset_split):
-    mrpc_dir = self._maybe_download_corpora(tmp_dir)
+    mrpc_dir = _maybe_download_corpora(tmp_dir)
     if dataset_split != problem.DatasetSplit.TEST:
       filesplit = "msr_paraphrase_train.txt"
     else:
@@ -134,3 +135,64 @@ class MSRParaphraseCorpusCharacters(MSRParaphraseCorpus):
 
   def global_task_id(self):
     return problem.TaskID.EN_SIM
+
+@registry.register_problem
+class MSRParaphraseCorpusGenerateParaphrase(text_problems.Text2TextProblem):
+
+  @property
+  def is_generate_per_split(self):
+    return True
+
+  @property
+  def dataset_splits(self):
+    return [{
+        "split": problem.DatasetSplit.TRAIN,
+        "shards": 10,
+    }, {
+        "split": problem.DatasetSplit.EVAL,
+        "shards": 1,
+    }, {
+        "split": problem.DatasetSplit.TEST,
+        "shards": 1,
+    }]
+
+  @property
+  def approx_vocab_size(self):
+    return 2**13
+
+  def example_generator(self, filename, dev_ids, dataset_split):
+    for idx, line in enumerate(tf.gfile.Open(filename, "rb")):
+      if idx == 0: continue  # skip header
+      line = text_encoder.to_unicode_utf8(line.strip())
+      split_line = line.split("\t")
+      if len(split_line) < 5:
+        continue
+      l, id1, id2, s1, s2 = split_line
+      if l == "0":
+        continue
+      is_dev = [id1, id2] in dev_ids
+      if dataset_split == problem.DatasetSplit.TRAIN and is_dev:
+        continue
+      if dataset_split == problem.DatasetSplit.EVAL and not is_dev:
+        continue
+      inputs = [[s1, s2], [s2, s1]]
+      for inp in inputs:
+        yield {
+            "inputs": inp[0],
+            "targets": inp[1]
+        }
+
+  def generate_samples(self, data_dir, tmp_dir, dataset_split):
+    mrpc_dir = _maybe_download_corpora(tmp_dir)
+    if dataset_split != problem.DatasetSplit.TEST:
+      filesplit = "msr_paraphrase_train.txt"
+    else:
+      filesplit = "msr_paraphrase_test.txt"
+    dev_ids = []
+    if dataset_split != problem.DatasetSplit.TEST:
+      for row in tf.gfile.Open(os.path.join(mrpc_dir, "dev_ids.tsv")):
+        dev_ids.append(row.strip().split("\t"))
+
+    filename = os.path.join(mrpc_dir, filesplit)
+    for example in self.example_generator(filename, dev_ids, dataset_split):
+      yield example
